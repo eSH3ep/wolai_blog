f58834d3112aad7d42e21b3e05fb2f09f4dd3656
eSH3ep <66587871+eSH3ep@users.noreply.github.com>
2023-04-15 13:06:17 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '8YFRkpkKaz6CF1gFhsMH92',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://www.wolai.com/8YFRkpkKaz6CF1gFhsMH92',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
