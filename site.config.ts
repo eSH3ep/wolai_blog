d31c68244f1a5f0235550e594806f5fd03b2cf14
eSH3ep <66587871+eSH3ep@users.noreply.github.com>
2023-04-15 15:26:34 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'BlazingD',
   domain: 'blazingD',
   author: 'BlazingDoggy',
 
